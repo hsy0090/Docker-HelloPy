e92cb2998456ba0e5eba0cade0d4dbc2f354255c
Hsy0090 <hsy0090.hsy@gmail.com>
2026-01-27 14:30:22 +0800

    Added Persist data (visit count)

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+COUNT_FILE = "/data/count.txt"
 
 @app.route("/")
 def hello():
+    if not os.path.exists("/data"):
+        os.makedirs("/data")
+
+    count = 0
+    if os.path.exists(COUNT_FILE):
+        count = int(open(COUNT_FILE).read())
+
+    count += 1
+    open(COUNT_FILE, "w").write(str(count))
+
     name = os.getenv("NAME", "Docker")
-    return f"Hello {name}!"
+
+    return f"Hello {name}! Visit #{count}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
