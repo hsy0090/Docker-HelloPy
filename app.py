d7a541a86f611bc46f9f51cde4b21c95914ba763
Hsy0090 <hsy0090.hsy@gmail.com>
2026-01-28 13:41:21 +0800

    Added Redis

--- app.py
+++ app.py
@@ -1,24 +1,14 @@
-import os
 from flask import Flask
+import redis
+import os
 
 app = Flask(__name__)
-COUNT_FILE = "/data/count.txt"
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 @app.route("/")
 def hello():
-    if not os.path.exists("/data"):
-        os.makedirs("/data")
-
-    count = 0
-    if os.path.exists(COUNT_FILE):
-        count = int(open(COUNT_FILE).read())
-
-    count += 1
-    open(COUNT_FILE, "w").write(str(count))
-
-    name = os.getenv("NAME", "Docker")
-
-    return f"Hello {name}! Visit #{count}"
+    count = r.incr("visits")
+    return f"Hello Docker! Redis visits: {count}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
